469897cfa97032c9a88d15e19c0b7ea629b7b5e1
Umar, Ismail Abiodun <ismail@ismailumar.com.ng>
2021-03-20 10:11:29 +0100

    Initial Commit

---
+++ PharmVerse/PharmVerse.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace PharmVerse.Tests.Unit
+{
+     public class Tests
+     {
+          [SetUp]
+          public void Setup()
+          {
+          }
+
+          [Test]
+          public void Test1()
+          {
+               Assert.Pass();
+          }
+     }
+}
