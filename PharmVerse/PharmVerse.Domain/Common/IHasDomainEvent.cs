f216341d9cace7d0c3d1e4d64e38f85a3e43bc02
Umar, Ismail Abiodun <ismail@ismailumar.com.ng>
2021-03-20 14:43:24 +0100

    Added Base Entity

---
+++ PharmVerse/PharmVerse.Domain/Common/IHasDomainEvent.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace PharmVerse.Domain.Common
+{
+     public interface IHasDomainEvent
+     {
+          public List<DomainEvent> DomainEvents { get; set; }
+     }
+}
