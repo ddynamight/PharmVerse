f5e28638d1bc87018a46d87046d6c2c2cf9fe4a6
TOCHUKWU ONYEAMAH <TochukwuOnyeamah@globusbank.com>
2021-05-20 14:39:15 +0100

    added other domain configurations to the pharmVerse persistence

---
+++ PharmVerse/PharmVerse.Persistence/Invites/Configurations/InviteEntityTypeConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PharmVerse.Domain.Invites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmVerse.Persistence.Invites.Configurations
+{
+    public class InviteEntityTypeConfiguration : IEntityTypeConfiguration<Invite>
+    {
+        public void Configure(EntityTypeBuilder<Invite> builder)
+        {
+            builder.ToTable("Invites");
+
+            builder.HasKey(i => i.AppUserId);
+            builder.HasKey(i => i.Id);
+            builder.Ignore(i => i.DomainEvents);
+
+            builder.Property(i => i.RowVersion).IsRowVersion();
+        }
+    }
+}
