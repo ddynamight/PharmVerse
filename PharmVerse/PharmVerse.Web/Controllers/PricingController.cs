c0ad4a202df00fe6f5bfe98c4f53f8b560952f30
Adeogun Damilola <adeogundamilolaaminat@gmail.com>
2021-06-17 19:14:06 +0100

    Added web app Added bootstrap and tailwind libraries Modified About, contact, faq, home and pricing controllers and created all their views also

--- PharmVerse/PharmVerse.Web/Controllers/PricingController.cs
+++ PharmVerse/PharmVerse.Web/Controllers/PricingController.cs
@@ -1,13 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace PharmVerse.Web.Controllers
 {
-    public class PricingController
+    public class PricingController : Controller
     {
-        
+        private readonly ILogger<PricingController> _logger;
+
+        public PricingController(ILogger<PricingController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
