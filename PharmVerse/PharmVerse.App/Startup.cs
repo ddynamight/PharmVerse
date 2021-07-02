7d9a5ae229e4f47c036d599d4982693cbccbc477
Adeogun Damilola <adeogundamilolaaminat@gmail.com>
2021-07-02 16:01:08 +0100

    Added navigation, side menu, footer and body to both admin and app

--- PharmVerse/PharmVerse.App/Startup.cs
+++ PharmVerse/PharmVerse.App/Startup.cs
@@ -16,25 +16,39 @@ namespace PharmVerse.App
           // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
           public void ConfigureServices(IServiceCollection services)
           {
-          }
+            services.AddMvc();
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+        }
 
           // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
           public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
           {
-               if (env.IsDevelopment())
-               {
-                    app.UseDeveloperExceptionPage();
-               }
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
 
-               app.UseRouting();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-               app.UseEndpoints(endpoints =>
-               {
-                    endpoints.MapGet("/", async context =>
-                 {
-                        await context.Response.WriteAsync("Hello World!");
-                   });
-               });
-          }
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
      }
 }
