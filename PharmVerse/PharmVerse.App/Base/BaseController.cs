486cd662cfd0439a5bb2ce6fdc5511b3ee45e62c
Umar, Ismail Abiodun <ismail@ismailumar.com>
2021-07-05 03:37:52 +0100

    updated app web project

---
+++ PharmVerse/PharmVerse.App/Base/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using PharmVerse.App.Services.Interfaces;
+using System.Collections.Generic;
+using System.Net.Http.Formatting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PharmVerse.App.Base
+{
+     public class BaseController : Controller
+     {
+          public IConfiguration Configuration { get; }
+          public readonly ILogger<BaseController> _logger;
+          public readonly IPharmVerseApiHttpClient _apiClient;
+
+          public BaseController(IConfiguration configuration, ILogger<BaseController> logger, IPharmVerseApiHttpClient apiClient)
+          {
+               Configuration = configuration;
+               _logger = logger;
+               _apiClient = apiClient;
+
+               formatters = new List<MediaTypeFormatter>() { new JsonMediaTypeFormatter(), new XmlMediaTypeFormatter() };
+          }
+
+          protected List<MediaTypeFormatter> formatters = new List<MediaTypeFormatter>();
+     }
+}
